5ca1d10592d5da688abaf96eb918b62875d1dcdc
ThantsinAg19 <thantsin1domark@gmail.com>
2020-11-25 13:35:26 +0630

    update git ignore

--- src/service/api.js
+++ src/service/api.js
@@ -1,16 +1,4 @@
-const BASE_URL_DEV = `http://localhost:4500/api/v1`
-// const BASE_URL_DEV = `http://khapi.herokuapp.com/api/v1`
-const BASE_URL_PROD = `http://live`
-
-function check_env () {
-    
-    if(process.env.NODE_ENV === 'production') 
-        return BASE_URL_PROD;
-    
-    return BASE_URL_DEV;
-}   
-
-export const BASE_URL = check_env();
+export const BASE_URL = process.env.REACT_APP_BASE_URL
 
 /**
  * USER
@@ -21,3 +9,16 @@ export const login = '/users/loginuser';
  * USER ROLE AND PERMISSION
  */
 export const userrole = '/userroles';
+
+/*
+const BASE_URL_DEV = `http://localhost:4500/api/v1`
+const BASE_URL_PROD = `http://live`
+
+function check_env () {
+    
+    if(process.env.NODE_ENV === 'production') 
+        return BASE_URL_PROD;
+    
+    return BASE_URL_DEV;
+}   
+*/
